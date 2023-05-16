d734bc1fe4173fce699ddec0f07be7f14635ba0e
mjovanc <mjovanc@icloud.com>
2023-05-16 23:53:09 +0200

    Adding Footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import "../app.css";
+  import Navbar from "../lib/components/Navbar.svelte";
+  import Footer from "../lib/components/Footer.svelte";
 </script>
 
+<Navbar />
+
 <slot />
+
+<Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#004E0C',
+      'yellow': '#ffc82c',
+      'black': '#181818',
+      'gray-dark': '#252E28',
+      'white': '#EBEBEB',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 };
