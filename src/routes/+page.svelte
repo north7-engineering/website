71e3a1d29a4701df385a5759a181b1e53b9493cd
mjovanc <mjovanc@icloud.com>
2023-05-17 01:59:02 +0200

    Adding css.gg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,28 +13,36 @@
   Trust us to deliver top-notch solutions that propel your organization forward.
 </p>
 
-<h2 class="mt-14 regular-font text-black uppercase dark:text-white">Web</h2>
-
+<div class="flex items-center">
+  <i class="gg-screen mt-14 mr-4 dark:text-white"></i>
+  <h2 class="mt-14 regular-font text-black uppercase dark:text-white">Web</h2>
+</div>
 <p class="mt-6 text-lg regular-font text-black dark:text-gray max-w-4xl">
   Our web development services harness cutting-edge technologies to create reliable and highly performant websites.
   With meticulous attention to detail and a focus on user experience, we build responsive and scalable web solutions tailored to your specific requirements.
   Trust us to deliver an exceptional online presence that engages and captivates your audience.
 </p>
 
-<h2 class="mt-14 regular-font text-black uppercase dark:text-white">System Development</h2>
-
+<div class="flex items-center">
+  <i class="gg-bolt mt-14 mr-4 dark:text-white"></i>
+  <h2 class="mt-14 regular-font text-black uppercase dark:text-white">System Development</h2>
+</div>
 <p class="mt-6 text-lg regular-font text-black dark:text-gray max-w-4xl">
   Leveraging our expertise in system development, we craft reliable and highly performant software solutions that optimize your business processes.
   Our team of skilled professionals designs and implements robust systems tailored to your unique needs, ensuring seamless integration and efficient workflows.
   Experience enhanced productivity and streamlined operations with our top-notch system development services.
 </p>
 
-<h2 class="mt-14 regular-font text-black uppercase dark:text-white">Integration</h2>
-
+<div class="flex items-center">
+  <i class="gg-list-tree mt-14 mr-4 dark:text-white"></i>
+  <h2 class="mt-14 regular-font text-black uppercase dark:text-white">Integration</h2>
+</div>
 <p class="mt-6 text-lg regular-font text-black dark:text-gray max-w-4xl">
   With our integration solutions, we seamlessly connect disparate systems and applications, enabling smooth data flow and enhanced functionality.
   Our experienced team leverages industry-leading tools and techniques to ensure reliable and highly performant integration, allowing your systems to work together seamlessly.
   Trust us to optimize your business processes through efficient data synchronization and streamlined operations.
 </p>
+
+
 
 
