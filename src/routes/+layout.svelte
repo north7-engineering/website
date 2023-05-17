3d5c63d9b5abe6c33a1ae3a85a132aad97b0a413
mjovanc <mjovanc@icloud.com>
2023-05-17 20:44:54 +0200

    Adding vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
   import "../app.css";
   import Navbar from "../lib/components/Navbar.svelte";
   import Footer from "../lib/components/Footer.svelte";
+
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navbar />
