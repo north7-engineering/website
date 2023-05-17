04981e84f03317dfaa74c4de5cca0be0bf5ef8bc
mjovanc <mjovanc@icloud.com>
2023-05-18 00:18:18 +0200

    Updating Twitter link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -15,7 +15,7 @@
                           <a href="https://github.com/north7-engineering" class="regular-font hover:underline dark:hover:text-green dark:text-white">GitHub</a>
                       </li>
                       <li class="mb-4">
-                          <a href="https://twitter.com/north7dev" class="regular-font hover:underline dark:hover:text-green dark:text-white">Twitter</a>
+                          <a href="https://twitter.com/north7eng" class="regular-font hover:underline dark:hover:text-green dark:text-white">Twitter</a>
                       </li>
                   </ul>
               </div>
@@ -34,7 +34,7 @@
           <span class="regular-font text-sm text-gray-500 sm:text-center dark:text-gray-400 dark:text-white">© 2023 North7 Engineering. All Rights Reserved.
           </span>
           <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-              <a href="https://twitter.com/north7dev" class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white">
+              <a href="https://twitter.com/north7eng" class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white">
                   <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
                   <span class="sr-only">Twitter page</span>
               </a>
