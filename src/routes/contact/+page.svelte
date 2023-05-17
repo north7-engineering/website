63cf0cd7faf138050c019eb7f9091c002dc46b85
mjovanc <mjovanc@icloud.com>
2023-05-17 17:26:02 +0200

    Updating contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,15 +10,15 @@
 
 <dl class="mt-14 max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
     <div class="flex flex-col pb-3">
-        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Email address</dt>
-        <dd class="text-lg font-semibold">marcus@icloud.com</dd>
+        <dt class="regular-font mb-1 text-gray md:text-lg dark:text-grey">Email address</dt>
+        <dd class="regular-font text-lg font-semibold dark:text-white">marcus@icloud.com</dd>
     </div>
     <div class="flex flex-col py-3">
-        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Home address</dt>
-        <dd class="text-lg font-semibold">Ripvägen 3, 352 42, Växjö, Sweden</dd>
+        <dt class="regular-font mb-1 text-gray md:text-lg dark:text-grey">Home address</dt>
+        <dd class="regular-font text-lg font-semibold dark:text-white">Ripvägen 3, 352 42, Växjö, Sweden</dd>
     </div>
     <div class="flex flex-col pt-3">
-        <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">Phone number</dt>
-        <dd class="text-lg font-semibold">+46 70 252 07 93</dd>
+        <dt class="regular-font mb-1 text-gray md:text-lg dark:text-grey">Phone number</dt>
+        <dd class="regular-font text-lg font-semibold dark:text-white">+46 70 252 07 93</dd>
     </div>
 </dl>
