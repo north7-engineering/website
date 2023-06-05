d2ab7e89266e082ad345f04f80d7ee41e7722016
mjovanc <mjovanc@icloud.com>
2023-06-05 16:20:06 +0200

    Adding link in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,49 +1,124 @@
 <footer class="bg-white dark:bg-black">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-              <a href="/" class="flex items-center">
-                  <img src="/logo/black.svg" class="h-10 mr-3 block dark:hidden" alt="North7 Logo" />
-                  <img src="/logo/white.svg" class="h-10 mr-3 hidden dark:block" alt="North7 Logo" />
-              </a>
-          </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
-              <div>
-                  <h2 class="regular-font mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-green">Follow us</h2>
-                  <ul class="text-gray-600 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="https://github.com/north7-engineering" class="regular-font hover:underline dark:hover:text-green dark:text-white">GitHub</a>
-                      </li>
-                      <li class="mb-4">
-                          <a href="https://twitter.com/north7eng" class="regular-font hover:underline dark:hover:text-green dark:text-white">Twitter</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="regular-font mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-green">Legal</h2>
-                  <ul class="text-gray-600 dark:text-gray-400 font-medium">
-                      <li>
-                          <a href="/cookies" class="regular-font hover:underline dark:hover:text-green dark:text-white">Cookies</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
+  <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <div class="md:flex md:justify-between">
+      <div class="mb-6 md:mb-0">
+        <a href="/" class="flex items-center">
+          <img
+            src="/logo/black.svg"
+            class="h-10 mr-3 block dark:hidden"
+            alt="North7 Logo"
+          />
+          <img
+            src="/logo/white.svg"
+            class="h-10 mr-3 hidden dark:block"
+            alt="North7 Logo"
+          />
+        </a>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-green lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="regular-font text-sm text-gray-500 sm:text-center dark:text-gray-400 dark:text-white">© 2023 North7 Engineering. All Rights Reserved.
-          </span>
-          <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-              <a href="https://twitter.com/north7eng" class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                  <span class="sr-only">Twitter page</span>
-              </a>
-              <a href="https://github.com/north7-engineering" class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white">
-                  <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
-                  <span class="sr-only">GitHub account</span>
-              </a>
-          </div>
+      <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
+        <div>
+          <h2
+            class="regular-font mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-green"
+          >
+            Follow us
+          </h2>
+          <ul class="text-gray-600 dark:text-gray-400 font-medium">
+            <li class="mb-4">
+              <a
+                href="https://github.com/north7-engineering"
+                class="regular-font hover:underline dark:hover:text-green dark:text-white"
+                >GitHub</a
+              >
+            </li>
+            <li class="mb-4">
+              <a
+                href="https://twitter.com/north7eng"
+                class="regular-font hover:underline dark:hover:text-green dark:text-white"
+                >Twitter</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2
+            class="regular-font mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-green"
+          >
+            Legal
+          </h2>
+          <ul class="text-gray-600 dark:text-gray-400 font-medium">
+            <li>
+              <a
+                href="/cookies"
+                class="regular-font hover:underline dark:hover:text-green dark:text-white"
+                >Cookies</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-green lg:my-8" />
+    <div class="sm:flex sm:items-center sm:justify-between">
+      <span
+        class="regular-font text-sm text-gray-500 sm:text-center dark:text-gray-400 dark:text-white"
+        >© 2023 North7 Engineering. All Rights Reserved.
+      </span>
+      <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+        <a
+          href="https://www.linkedin.com/company/north7-engineering"
+          class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white"
+        >
+          <svg
+            class="w-5 h-5"
+            height="50"
+            viewBox="0 0 72 72"
+            width="50"
+            xmlns="http://www.w3.org/2000/svg"
+            ><g fill="none" fill-rule="evenodd"
+              ><path
+                d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
+                fill="currentColor"
+              /><path
+                d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
+                fill="#FFF"
+              /></g
+            ></svg
+          >
+          <span class="sr-only">LinkedIn</span>
+        </a>
+        <a
+          href="https://twitter.com/north7eng"
+          class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            ><path
+              d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+            /></svg
+          >
+          <span class="sr-only">Twitter page</span>
+        </a>
+        <a
+          href="https://github.com/north7-engineering"
+          class="regular-font text-gray-500 hover:text-gray-900 dark:hover:text-green dark:text-white"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            ><path
+              fill-rule="evenodd"
+              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+              clip-rule="evenodd"
+            /></svg
+          >
+          <span class="sr-only">GitHub account</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </footer>
-
