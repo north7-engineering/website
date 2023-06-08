75366cddfab7ed38b5b28e311a2581dc26d0e147
mjovanc <mjovanc@icloud.com>
2023-06-08 08:45:51 +0200

    Updating footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
-<script lang="ts">
+<script>
   import "../app.css";
   import Navbar from "../lib/components/Navbar.svelte";
   import Footer from "../lib/components/Footer.svelte";
 
-  import { dev } from '$app/environment';
-  import { inject } from '@vercel/analytics';
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
 
-  inject({ mode: dev ? 'development' : 'production' });
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <Navbar />
 
 <div class="mx-auto my-10 w-full max-w-screen-xl p-4 py-6 lg:py-8">
-<slot />
+  <slot />
 </div>
 
 <Footer />
